9749e29ffa11094326b721ec48fa6a7c98b356ae
jiaozhu <jiaozhu@njust.edu.cn>
2020-10-14 10:17:48 +0800

    add TwoStepMOT

--- tools/mot_utils/__init__.py
+++ tools/mot_utils/__init__.py
@@ -3,8 +3,9 @@ from .build import (build_tracktor_encoder,
 
 from .sequence_dataset import build_dataset
 from .one_shot_handler import OneShotMOT
+from .two_step_handler import TwoStepMOT
 
 __all__ = [
     'build_tracktor_encoder', 'build_detect_encoder', 'build_embed_encoder',
-    'build_dataset', 'OneShotMOT'
+    'build_dataset', 'OneShotMOT', 'TwoStepMOT'
 ]
